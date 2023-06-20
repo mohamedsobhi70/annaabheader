57e419f70fda1734c2629f5c08f42b89f8854159
mohamedsobhi70 <sobhi7070@gmail.com>
2023-06-20 15:09:13 +0300

    add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,41 +1,21 @@
 
-// Drop Down In Header 
-if ($(".dropdown-list").length) {
-    $(".dropdown-list").hover(function (e) {
-        $(".colored-act").removeClass("active")
-        if ($(this).find(".dropdown-list-items").hasClass("active")) {
-            $(".dropdown-list-items").removeClass("active");
-            $(this).removeClass("active")
+
+if ($(".menu-dropdown").length) {
+
+    $(".menu-dropdown").on("mouseenter", function () {
+        if ($(window).width() > 1024) {
+            $(this).find(".menu-dropdown-list").addClass("active");
         }
-        else {
-            $(".dropdown-list-items").removeClass("active");
-            $(this).find(".dropdown-list-items").addClass("active");
-            $(this).addClass("active")
+    }).on("mouseleave", function () {
+        if ($(window).width() > 1024) {
+            $(this).find(".menu-dropdown-list").removeClass("active");
+        }
+    }).on("click", function () {
+        if ($(window).width() < 1023) {
+            $(this).find(".menu-dropdown-list").slideToggle(500);
         }
     })
-}
 
-// mobile dropdown 
-if ($(".menu-dropdown").length) {
-    if ($(window).width() < 1023) {
-        $(".menu-dropdown").on("click", function (e) {
-            $(this).find(".menu-dropdown-list").slideToggle(500);
-        })
-    }
-    else{
-        $(".menu-dropdown").hover(function (e) {
-            $(".colored-act").removeClass("active")
-            if ($(this).find(".menu-dropdown-list").hasClass("active")) {
-                $(".menu-dropdown-list").removeClass("active");
-                $(this).removeClass("active")
-            }
-            else {
-                $(".menu-dropdown-list").removeClass("active");
-                $(this).find(".menu-dropdown-list").addClass("active");
-                $(this).addClass("active")
-            }
-        })
-    }
 }
 // =================================================
 
