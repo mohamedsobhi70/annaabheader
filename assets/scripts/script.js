eddcc9c07d282c403170b449dc11682c86f2d87c
mohamedsobhi70 <sobhi7070@gmail.com>
2023-06-20 15:32:21 +0300

    add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,14 +5,17 @@ if ($(".menu-dropdown").length) {
     $(".menu-dropdown").on("mouseenter", function () {
         if ($(window).width() > 1024) {
             $(this).find(".menu-dropdown-list").addClass("active");
+            $(this).find(".chevron").addClass("rotat")
         }
     }).on("mouseleave", function () {
         if ($(window).width() > 1024) {
+            $(this).find(".chevron").removeClass("rotat")
             $(this).find(".menu-dropdown-list").removeClass("active");
         }
     }).on("click", function () {
         if ($(window).width() < 1023) {
             $(this).find(".menu-dropdown-list").slideToggle(500);
+            $(this).find(".chevron").toggleClass("rotat");            
         }
     })
 
